020f42ecec9952e6407aa8c03348377bf0427589
Yoni Nachmany <nachmany@seas.upenn.edu>
2016-02-13 09:29:25 -0500

    Add /add_job endpoint which takes a POST containing just a audio url (for now)

--- main.go
+++ main.go
@@ -5,12 +5,14 @@ import (
 	"net/http"
 	_ "net/http/pprof" // import for side effects
 
+	"encoding/json"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/hello/{name}", helloHandler)
+	r.HandleFunc("/add_job", jobHandler)
 
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
@@ -19,4 +21,20 @@ func main() {
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	args := mux.Vars(r)
 	fmt.Fprintf(w, "Hello %s!", args["name"])
+}
+
+func jobHandler(w http.ResponseWriter, r *http.Request) {
+	var d audioData
+
+	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	json.NewEncoder(w).Encode(d)
+}
+
+type audioData struct {
+	AudioURL string `json:"audioURL"`
 }
