532409a992f5d37baffbd65ca6910fcf076e6f5d
Yoni Nachmany <nachmany@seas.upenn.edu>
2016-02-14 01:32:04 -0500

    Changed StatusInternalServerError to StatusBadRequest

--- main.go
+++ main.go
@@ -34,19 +34,17 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 // initiateTranscriptionJobHandle takes a POST request containing a json object,
-// decodes it into an audioData struct, and returns the struct json-encoded.
+// decodes it into an audioData struct, and returns appropriate message.
 func initiateTranscriptionJobHandler(w http.ResponseWriter, r *http.Request) {
 	var jsonData transcriptionJobData
 
 	// unmarshal from the response body directly into our struct
 	if err := json.NewDecoder(r.Body).Decode(&jsonData); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	// return the struct encoded as json
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	json.NewEncoder(w).Encode(jsonData)
+	fmt.Fprintf(w, "Accepted!")
 }
 
 type transcriptionJobData struct {
