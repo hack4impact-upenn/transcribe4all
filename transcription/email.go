13e52ec8992e439dc004e255eb20b9032411d982
Yoni Nachmany <nachmany@seas.upenn.edu>
2016-02-25 10:16:10 -0500

    Removed CC in argument bc a little confused about difference between cc and to arrays; set username to 'from' for clarity

--- transcription/email.go
+++ transcription/email.go
@@ -1,23 +1,22 @@
 package transcription
 
 import (
-	"log"
 	"net/smtp"
 	"strings"
 )
 
 // SendEmail authenticates, creates message, and sends email from arguments.
-func SendEmail(username string, password string, host string, port int, to []string, subject string, cc []string, body string) {
+func SendEmail(username string, password string, host string, port int, to []string, subject string, body string) error {
 	auth := smtp.PlainAuth("", username, password, host)
 
 	// The msg headers should usually include fields such as "From", "To", "Subject", and "Cc".
 	// Sending "Bcc" messages is accomplished by including an email address in
 	// the to parameter but not including it in the msg headers.
-	fromHeader := "From: " + username
+	from := username
+	fromHeader := "From: " + from
 	toHeader := "To: " + strings.Join(to, ", ")
 	subjectHeader := "Subject: " + subject
-	ccHeader := "Cc: " + strings.Join(cc, ", ")
-	msgHeaders := []string{fromHeader, toHeader, subjectHeader, ccHeader}
+	msgHeaders := []string{fromHeader, toHeader, subjectHeader}
 
 	// The msg parameter should be an RFC 822-style email with headers first,
 	// a blank line, and then the message body. The lines of msg should be CRLF terminated.
@@ -25,8 +24,8 @@ func SendEmail(username string, password string, host string, port int, to []str
 		"\r\n" +
 		body + "\r\n")
 	addr := host + ":" + string(port)
-	err := smtp.SendMail(addr, auth, username, to, msg)
-	if err != nil {
-		log.Fatal(err)
+	if err := smtp.SendMail(addr, auth, from, to, msg); err != nil {
+		return err
 	}
+	return nil
 }
