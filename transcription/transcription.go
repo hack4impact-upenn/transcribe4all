a4a9f28a6b660ca30abcd368c61f70f950d1bd5c
Yoni Nachmany <nachmany@seas.upenn.edu>
2016-02-25 18:16:24 -0500

    Pulled logic out of setting headers, new function returns str version; documented meaning of host and port arguments

--- transcription/transcription.go
+++ transcription/transcription.go
@@ -5,25 +5,27 @@ import (
 	"strings"
 )
 
-// SendEmail authenticates, creates message, and sends email from arguments.
+// SendEmail authenticates, creates message, and sends email using arguments.
+// Host is email server, e.g. "smtp.gmail.com". Port of server required, e.g. 25.
 func SendEmail(username string, password string, host string, port int, to []string, subject string, body string) error {
-	auth := smtp.PlainAuth("", username, password, host)
-
-	// The msg headers should usually include fields such as "From", "To", "Subject", and "Cc".
 	from := username
-	fromHeader := "From: " + from
-	toHeader := "To: " + strings.Join(to, ", ")
-	subjectHeader := "Subject: " + subject
-	msgHeaders := []string{fromHeader, toHeader, subjectHeader}
+	auth := smtp.PlainAuth("", username, password, host)
 
 	// The msg parameter should be an RFC 822-style email with headers first,
 	// a blank line, and then the message body. The lines of msg should be CRLF terminated.
-	msg := []byte(strings.Join(msgHeaders, "\r\n") +
-		"\r\n" +
-		body + "\r\n")
+	msg := []byte(msgHeaders(from, to, subject) + "\r\n" + body + "\r\n")
 	addr := host + ":" + string(port)
 	if err := smtp.SendMail(addr, auth, from, to, msg); err != nil {
 		return err
 	}
 	return nil
+}
+
+// The msg headers should usually include fields such as "From", "To", "Subject", and "Cc".
+func msgHeaders(from string, to []string, subject string) string {
+	fromHeader := "From: " + from
+	toHeader := "To: " + strings.Join(to, ", ")
+	subjectHeader := "Subject: " + subject
+	msgHeaders := []string{fromHeader, toHeader, subjectHeader}
+	return strings.Join(msgHeaders, "\r\n")
 }
