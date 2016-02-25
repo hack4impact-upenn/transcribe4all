9ee3fa8bf6f63f13573ed1cd55c0cc583b66da8b
Yoni Nachmany <nachmany@seas.upenn.edu>
2016-02-25 10:07:40 -0500

    Added more parameters to take in username, password, host info and header info; standardized headers for cleaner msg

--- transcription/email.go
+++ transcription/email.go
@@ -3,23 +3,29 @@ package transcription
 import (
 	"log"
 	"net/smtp"
-	"os"
 	"strings"
 )
 
-// SendEmail authenticates from environment variables and sends email using arguments.
-func SendEmail(from string, to []string, subject string, body string) {
-	// Set up authentication information.
-	email := os.Getenv("MAIL_EMAIL")
-	password := os.Getenv("MAIL_PASSWORD")
-	auth := smtp.PlainAuth("", email, password, "smtp.gmail.com")
+// SendEmail authenticates, creates message, and sends email from arguments.
+func SendEmail(username string, password string, host string, port int, to []string, subject string, cc []string, body string) {
+	auth := smtp.PlainAuth("", username, password, host)
 
-	msg := []byte("To: " + strings.Join(to, ", ") + "\r\n" +
-		"Subject: " + subject + "\r\n" +
+	// The msg headers should usually include fields such as "From", "To", "Subject", and "Cc".
+	// Sending "Bcc" messages is accomplished by including an email address in
+	// the to parameter but not including it in the msg headers.
+	fromHeader := "From: " + username
+	toHeader := "To: " + strings.Join(to, ", ")
+	subjectHeader := "Subject: " + subject
+	ccHeader := "Cc: " + strings.Join(cc, ", ")
+	msgHeaders := []string{fromHeader, toHeader, subjectHeader, ccHeader}
+
+	// The msg parameter should be an RFC 822-style email with headers first,
+	// a blank line, and then the message body. The lines of msg should be CRLF terminated.
+	msg := []byte(strings.Join(msgHeaders, "\r\n") +
 		"\r\n" +
 		body + "\r\n")
-	err := smtp.SendMail("smtp.gmail.com:25", auth, from, to, msg)
-
+	addr := host + ":" + string(port)
+	err := smtp.SendMail(addr, auth, username, to, msg)
 	if err != nil {
 		log.Fatal(err)
 	}
