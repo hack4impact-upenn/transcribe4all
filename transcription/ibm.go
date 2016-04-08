af838981c746295b0d93a4e79f3b5cee37afe310
Ben Sandler <ben1sandler@gmail.com>
2016-04-08 19:17:38 -0400

    Updated ibm transcription to use gorilla websockets; now working

--- transcription/ibm.go
+++ transcription/ibm.go
@@ -2,98 +2,56 @@ package transcription
 
 import (
 	"bufio"
-	"encoding/json"
+	"encoding/base64"
 	"io"
-	"io/ioutil"
-	"log"
 	"net/http"
-	"net/url"
 	"os"
+	"strings"
 
-	"golang.org/x/net/websocket"
+	"github.com/gorilla/websocket"
 )
 
 // TranscribeWithIBM transcribes a given audio file using the IBM Watson
 // Speech To Text API
 func TranscribeWithIBM(filePath string, IBMUsername string, IBMPassword string) (string, error) {
-	IBMAuthToken, err := getIBMAuthToken(IBMUsername, IBMPassword)
+	url := "wss://stream.watsonplatform.net/speech-to-text/api/v1/recognize?model=es-ES_BroadbandModel"
+	header := http.Header{}
+	header.Set("Authorization", "Basic "+basicAuth(IBMUsername, IBMPassword))
 
-	url, err := generateIBMURL(IBMAuthToken)
-	if err != nil {
-		return "", err
-	}
-	ws, err := websocket.Dial(url, "", "http://localhost:8000")
+	dialer := websocket.DefaultDialer
+	ws, _, err := dialer.Dial(url, header)
 	if err != nil {
 		return "", err
 	}
 	defer ws.Close()
 
-	requestArgs, err := json.Marshal(map[string]string{
+	requestArgs := map[string]interface{}{
 		"action":           "start",
 		"content-type":     "audio/flac",
-		"continuous":       "true",
-		"word_confidence":  "true",
-		"timestamps":       "true",
-		"profanity_filter": "false",
-		"interim_results":  "false",
-	})
-	if err != nil {
+		"continuous":       true,
+		"word_confidence":  true,
+		"timestamps":       true,
+		"profanity_filter": false,
+		"interim_results":  false,
+	}
+	if err = ws.WriteJSON(requestArgs); err != nil {
 		return "", err
 	}
-	if _, err = ws.Write(requestArgs); err != nil {
-		return "", err
-	}
-
 	if err = uploadBinaryWithWebsocket(ws, filePath); err != nil {
 		return "", err
 	}
-	log.Println("File uploaded")
 
-	ws.Write([]byte{}) // write empty message to indicate end of uploading file
-
+	ws.WriteMessage(websocket.BinaryMessage, []byte{}) // write empty message to indicate end of uploading file
 	transcriptionRes, err := pollForTranscriptionResult(ws)
 	if err != nil {
 		return "", err
 	}
-
 	return transcriptionRes, nil
 }
 
-func getIBMAuthToken(IBMUsername string, IBMPassword string) (string, error) {
-	baseURL, err := url.Parse("https://stream.watsonplatform.net/authorization/api/v1/token")
-	if err != nil {
-		return "", err
-	}
-	params := url.Values{}
-	params.Add("url", "https://stream.watsonplatform.net/speech-to-text/api")
-	baseURL.RawQuery = params.Encode()
-
-	req, err := http.NewRequest(http.MethodGet, baseURL.String(), nil)
-	if err != nil {
-		return "", err
-	}
-	req.SetBasicAuth(IBMUsername, IBMPassword)
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return "", err
-	}
-	token, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-	return string(token), nil
-}
-
-func generateIBMURL(IBMAuthToken string) (string, error) {
-	baseURL, err := url.Parse("wss://stream.watsonplatform.net/speech-to-text/api/v1/recognize")
-	if err != nil {
-		return "", err
-	}
-	params := url.Values{}
-	params.Add("watson-token", IBMAuthToken)
-	params.Add("model", "en-US_BroadbandModel")
-	baseURL.RawQuery = params.Encode()
-	return baseURL.String(), nil
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
 }
 
 func uploadBinaryWithWebsocket(ws *websocket.Conn, filePath string) error {
@@ -113,22 +71,20 @@ func uploadBinaryWithWebsocket(ws *websocket.Conn, filePath string) error {
 		if err != nil && err != io.EOF {
 			return err
 		}
-		ws.Write(buffer)
+		ws.WriteMessage(websocket.BinaryMessage, buffer)
 	}
 	return nil
 }
 
 func pollForTranscriptionResult(ws *websocket.Conn) (string, error) {
-	transcriptionRes := []byte{}
 	for {
-		log.Println("Waiting...")
-		n, err := ws.Read(transcriptionRes)
+		_, transcriptionRes, err := ws.ReadMessage()
 		if err != nil {
 			return "", err
 		}
-		if n > 0 {
-			break
+		// BUG(sandlerben): This is a hack which will not work if the transcription contains "listening"
+		if len(transcriptionRes) > 0 && !strings.Contains(string(transcriptionRes), "listening") {
+			return string(transcriptionRes), nil
 		}
 	}
-	return string(transcriptionRes), nil
 }
